39ed5d91868567b4242a691dd1a66e0b45b675fc
David García Díaz <davidgd472@gmail.com>
2022-12-28 12:35:00 +0000

    Trabajado en controller

--- src/main/java/es/ulpgc/is/model/Controller.java
+++ src/main/java/es/ulpgc/is/model/Controller.java
@@ -2,4 +2,16 @@ package es.ulpgc.is.model;
 
 public class Controller {
 
+    private PaymentManager paymentManager;
+    private DriverRepository driverRepository;
+    private ReserveTripRepository reserveTripRepository;
+    private PastTripRepository pastTripRepository;
+
+    public Controller() {
+        paymentManager = new PaymentManager();
+        driverRepository = new DriverRepository();
+        pastTripRepository = new PastTripRepository();
+        reserveTripRepository = new ReserveTripRepository();
+    }
+
 }
