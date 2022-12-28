d939095bcaaa9fde0bca56a5fa69522641007fb8
David García Díaz <davidgd472@gmail.com>
2022-12-28 11:47:14 +0000

    Trabajado en las clases

---
+++ src/main/java/es/ulpgc/is/Main.java
@@ -0,0 +1,11 @@
+package es.ulpgc.is;
+
+import es.ulpgc.is.model.Controller;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Controller controller = new Controller();
+
+	}
+}
--- src/main/java/es/ulpgc/is/app/Main.java
+++
@@ -1,7 +0,0 @@
-package es.ulpgc.is.app;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
